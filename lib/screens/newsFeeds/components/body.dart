c401cbc085f0d34bfcbbf9c09b6837831240ad5c
Samyush <samyushmaharjan@gmail.com>
2021-02-22 12:38:41 +0545

    homePage changes

--- lib/screens/newsFeeds/components/body.dart
+++ lib/screens/newsFeeds/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'buttonFunctionality.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,22 +16,25 @@ class Body extends StatelessWidget {
               image: DecorationImage(
                   image: AssetImage("images/wallPP.jpg"), fit: BoxFit.cover),
             ),
-            child: Center(
-              child: Image.asset(
-                'images/anupaRestro.png',
-                // height: queryData.size.height / 1.8,
-                // width: queryData.size.width / 1.8,
-              ),
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Image.asset(
+                    'images/anupaRestro.png',
+                    // height: queryData.size.height / 1.8,
+                    // width: queryData.size.width / 1.8,
+                  ),
+                ),
+                //
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: ButtonFunctionality(),
+                ),
+              ],
             ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('hello'),
-            Text('hello'),
-            Text('hello'),
-          ],
         ),
       ],
     );
