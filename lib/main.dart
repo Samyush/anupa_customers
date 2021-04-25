4c22bce1663853d8bb08666065c52953be1cdd55
Samyush <samyushmaharjan@gmail.com>
2021-04-25 23:55:57 +0545

    error encountered

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:anupa_customers/screens/splashScreen/splashDisplay.dart';
 import 'package:anupa_customers/theme.dart';
 import 'package:flutter/material.dart';
+//error in table rendering
 
 void main() => runApp(MyApp());
 
